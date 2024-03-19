583245259a0cb53a8d5f94aec9d8bbc1d124bb05
alonsozambrana97@gmail.com <alonsozambrana97@gmail.com>
2024-03-18 21:03:09 -0300

    tde1 laboratorio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿byte idade = 35; 
+Console.WriteLine(idade);
+string nome = ("Maria");
+Console.WriteLine(nome);
+float altura = 3.45f;
+Console.WriteLine(altura);
+const int ano = 12; 
+Console.WriteLine(ano);
+double? nota = 7.80;
+Console.WriteLine(nota);
+
+/*6) Variáveis de tipos de referência armazenam referências em seus dados (objetos) enquanto que variáveis de tipos de valor contém diretamente seus dados.
+7)Nullable Types serve para atribuir o valor null a um tipo de dado que não aceita valores nulos.
+8) Camel Case é um padrão usado onde a primeira palavra tem a letra minuscula e as demais comecam com letra maisucula. Exemplo. numeroVinteQuatro
+9)Pascal Case é um padrão em que todas palavras começam com letra maiúscula. Exemplo: AulaDoisLaboratório.*/
+
+int x = 77;
+int y = 66;
+Console.WriteLine(x+y);
+
+/*
+bool = False 
+int = 0
+char: \0
+double: 0.0
+float: 0.0f
+decimal: 0.0m
+string: null*/
